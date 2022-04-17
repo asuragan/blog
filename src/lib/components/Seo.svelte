7c69c2255ef2de3058109712825c2baa36d7c6ee
asuragan <55428472+asuragan@users.noreply.github.com>
2022-04-17 21:06:59 +0300

    Add files via upload

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let title = undefined;
+  export let description = undefined;
+  export let keywords = undefined;
+  export let canonical = undefined;
+</script>
+
+<svelte:head>
+  {#if title}
+    <title>{title}</title>
+  {/if}
+
+  <meta
+    name="robots"
+    content="noindex,nofollow,noarchive,noodp"
+  />
+  <meta 
+	name="googlebot" 
+	content="nosnippet"
+  />
+  
+
+  {#if description}
+    <meta name="description" content={description} />
+  {/if}
+
+  {#if canonical}
+    <link rel="canonical" href={canonical} />
+  {/if}
+
+  {#if keywords}
+    <meta name="keywords" content={keywords} />
+  {/if}
+
+  <slot />
+</svelte:head>
