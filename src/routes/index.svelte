7ff292e4d43a30ce6fa11ffd64a1cf495e6a6202
asuragan <55428472+asuragan@users.noreply.github.com>
2022-04-18 05:59:37 +0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,27 @@
 </script>
 
 <script>
+    import { onMount } from 'svelte';
+    import auth from '$lib/services/auth';
+    import { isAuthenticated, user } from '$lib/stores/auth';
+
+    let auth0Client;
+
+    onMount(async () => {
+        isAuthenticated.set(await auth0Client.isAuthenticated());
+        user.set(await auth0Client.getUser());
+    });
+
+    function login() {
+        auth.loginWithPopup(auth0Client);
+    }
+
+    function logout() {
+        auth.logout(auth0Client);
+    }
+</script>
+
+<script>
   import PostItem from "$lib/components/PostItem.svelte";
   import Seo from "$lib/components/Seo.svelte";
   import { siteTitle, siteDescription } from "$lib/constants";
@@ -23,7 +44,11 @@
 </script>
 
 <Seo {...seo} />
-
-{#each posts as post}
-  <PostItem {post} />
-{/each}
+{#if $isAuthenticated}
+<button on:click={logout}>Logout</button>
+  {#each posts as post}
+    <PostItem {post} />
+  {/each}
+{:else}
+    <button on:click={login}>Login</button>
+{/if}
