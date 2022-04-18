336c30d99d1de38225f70c2df71f75c8ee1c6f68
asuragan <55428472+asuragan@users.noreply.github.com>
2022-04-18 18:31:17 +0300

    Update stores.js

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,4 @@
 import { writable } from "svelte/store";
 
 export const isSidebarOpen = writable(false)
-export const isAuthenticated = writable(false);
-export const user = writable({});
-export const popupOpen = writable(false);
-export const error = writable();
+export const isLoggedIn = writable(false);
