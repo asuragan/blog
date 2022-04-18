650c0831fa1a3077441bc1d5c16e1627496f0b08
asuragan <55428472+asuragan@users.noreply.github.com>
2022-04-18 21:37:50 +0300

    Update loginForm.svelte

--- src/lib/components/loginForm.svelte
+++ src/lib/components/loginForm.svelte
@@ -38,9 +38,6 @@
             aria-describedby="emailHelp"
             placeholder="Email Address"
           />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
-            </div>
         </div>
         <div class="mb-3">
           <label for="passInput" class="form-label">Password</label>
@@ -60,7 +57,7 @@
 <style>
   .card {
     width: 50%;
-    margin: 0 auto;
+    margin: 1rem auto;
   }
   .login {
     margin-top: 50px;
