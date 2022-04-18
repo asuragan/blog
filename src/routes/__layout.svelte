551afb2f0e12da22a9b3c01238083f006fb4e0d9
asuragan <55428472+asuragan@users.noreply.github.com>
2022-04-18 18:44:53 +0300

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,3 @@
-<script context="module">
-  export const load = async ({ fetch }) => {
-    // Use a `limit` querystring parameter to fetch a limited number of posts
-    // e.g. fetch('posts.json?limit=5') for 5 most recent posts
-    const res = await fetch("/api/posts.json");
-    const posts = await res.json();
-
-    return {
-      stuff: {
-        posts,
-      },
-    };
-  };
-</script>
-
 <script>
   import "../../static/reset.css";
   import "../../static/global.css";
@@ -39,6 +24,21 @@
   });
   
   let fontSize = 1.15;
+</script>
+
+<script context="module">
+  export const load = async ({ fetch }) => {
+    // Use a `limit` querystring parameter to fetch a limited number of posts
+    // e.g. fetch('posts.json?limit=5') for 5 most recent posts
+    const res = await fetch("/api/posts.json");
+    const posts = await res.json();
+
+    return {
+      stuff: {
+        posts,
+      },
+    };
+  };
 </script>
 
 <div class="wrapper overflow-hidden">
